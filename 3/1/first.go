47dc01db52d5396af7d03cfa40717bb48fdbb972
whey86 <erikliu86@gmail.com>
2018-12-03 22:09:03 +0100

    3.1 ongoing

--- 3/1/first.go
+++ 3/1/first.go
@@ -9,6 +9,12 @@ import (
 	"strings"
 )
 
+var pattern [][]string
+var TAKEN string = "@"
+var OVERRITTEN = "X"
+var EMPTY string = "."
+var takenCounter int = 0
+
 func importTextFile(textFile string) string {
 	b, err := ioutil.ReadFile(textFile)
 	if err != nil {
@@ -17,58 +23,83 @@ func importTextFile(textFile string) string {
 	return string(b)
 }
 
-func buildCommandList (string input) command[] {
-	var commands[]command;
-	var scanner Scanner= bufio.NewScanner(strings.NewReader(input))
-	re := regexp.MustCompile("(\\d+)")
-	for scanner.Scan() {
-		var line string = scanner.Text();
-		var match[]string = re.FindAllString(line, -1);
-		commands := append(commands, commad{start[0]:match[1]});
-	}
-	return commands;
-}
-type command struct {
-	 start [2]int;
-	 end [2]int;
-	 sign string;
-}
-
-func main() {
-	fmt.Println("Advent of code day 1 part 1")
-	var sum int = 0
-	var frq []int
-	input := importTextFile("first.txt")
-
-	re := regexp.MustCompile("[0-9]+")
-
+func getStringArray() []string {
+	input := importTextFile("test.txt")
+	var arrayOflines []string
 	scanner := bufio.NewScanner(strings.NewReader(input))
 
 	for scanner.Scan() {
-		var myString = scanner.Text()
-		//fmt.Println(myString);
-		frq = append(frq, sum)
-		if strings.Contains(myString, "+") {
-
-			testmatch := re.FindStringSubmatch(myString)
-			extractInt, err := strconv.Atoi(testmatch[0])
-			if err != nil {
-				// handle error
-				fmt.Println(err)
-			}
-			sum += extractInt
-
-		} else {
-			testmatch := re.FindStringSubmatch(myString)
-			extractInt, err := strconv.Atoi(testmatch[0])
-			if err != nil {
-				// handle error
-				fmt.Println(err)
-			}
-			sum -= extractInt
-
-		}
-
+		var line = scanner.Text()
+		arrayOflines = append(arrayOflines, line)
 	}
-	fmt.Println("Frequecy is: ", sum)
+	return arrayOflines
+
+}
+
+func buildCommandList(input []string) []command {
+	var commands []command
+
+	for i := 0; i < len(input); i++ {
+		re := regexp.MustCompile("(\\d+)")
+		var line string = input[i]
+		var match []string = re.FindAllString(line, -1)
+		ex, _ := strconv.Atoi(match[3])
+		sx, _ := strconv.Atoi(match[1])
+		ey, _ := strconv.Atoi(match[4])
+		sy, _ := strconv.Atoi(match[2])
+		commands = append(commands, command{
+			startX: sx,
+			startY: sy,
+			endX:   ex + sx,
+			endY:   ey + sy})
+	}
+
+	return commands
+}
+
+func addToPattern(x int, y int) {
+	horizontalSize := len(pattern)
+
+	//if array does not exist
+	if horizontalSize < x {
+		for i := horizontalSize; i <= x; i++ {
+			pattern = append(pattern, []string{})
+			for j := 0; j <= y; j++ {
+				pattern[i] = append(pattern[i], ".")
+			}
+		}
+	}
+	currentPattern := pattern[x][y]
+	switch p := currentPattern; p {
+	case EMPTY:
+		pattern[x][y] = TAKEN
+	case TAKEN:
+		pattern[x][y] = OVERRITTEN
+		takenCounter++
+	}
+
+}
+
+type command struct {
+	startX int
+	endX   int
+	startY int
+	endY   int
+	sign   string
+}
+
+func main() {
+	fmt.Println("Advent of code day 3 part 1")
+	commands := buildCommandList(getStringArray())
+
+	for _, element := range commands {
+
+		for i := element.endX; i >= element.startX; i-- {
+			for j := element.endY; j >= element.startY; j-- {
+				addToPattern(i, j)
+			}
+		}
+	}
+
+	fmt.Println("Number of overritten ", takenCounter)
 }
